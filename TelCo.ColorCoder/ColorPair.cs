5f2e1fcf1cef87dc6f8d96d148c6d90bc6b8fd5a
Rizwan Nargundkar <UmarShaikMNargundkar.Rizwan@in.bosch.com>
2021-02-22 20:38:34 +0530

    Making file lines less than 50

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -13,5 +13,28 @@ namespace TeleCommunication.ColorCoder
         {
             return string.Format("MajorColor:{0}, MinorColor:{1}", MajorColor.Name, MinorColor.Name);
         }
+
+        public static ColorPair GetColorFromPairNumber(int pairNumber)
+        {
+            int minorSize = ColorMap.Minor.Length;
+            int majorSize = ColorMap.Major.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+            }
+
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
+            ColorPair pair = new ColorPair()
+            {
+                MajorColor = ColorMap.Major[majorIndex],
+                MinorColor = ColorMap.Minor[minorIndex]
+            };
+
+            return pair;
+        }
     }
 }
--- TelCo.ColorCoder/ColorPair2.cs
+++ TelCo.ColorCoder/ColorPair2.cs
@@ -6,29 +6,6 @@ namespace TeleCommunication.ColorCoder
 {
     public partial class ColorPair
     {
-        public static ColorPair GetColorFromPairNumber(int pairNumber)
-        {
-            int minorSize = ColorMap.Minor.Length;
-            int majorSize = ColorMap.Major.Length;
-            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
-            {
-                throw new ArgumentOutOfRangeException(
-                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
-            }
-
-            int zeroBasedPairNumber = pairNumber - 1;
-            int majorIndex = zeroBasedPairNumber / minorSize;
-            int minorIndex = zeroBasedPairNumber % minorSize;
-
-            ColorPair pair = new ColorPair()
-            {
-                MajorColor = ColorMap.Major[majorIndex],
-                MinorColor = ColorMap.Minor[minorIndex]
-            };
-
-            return pair;
-        }
-
         public static int GetPairNumberFromColor(ColorPair pair)
         {
             int majorIndex = -1, minorIndex = -1;
